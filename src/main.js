7e87b0b68dc15d9071ef7bea85e820c69ad22034
陳国偉 <guowei.chen@hh-c.co.jp>
2021-07-21 12:52:27 +0900

    環境作成

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import installElementPlus from './plugins/element';
+import installAxios from './plugins/axios';
+
+const app = createApp(App);
+installElementPlus(app);
+installAxios(app);
+
+app.use(store).use(router).mount('#app');
---
+++ src/plugins/axios.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+export default (app) => {
+  app.use(VueAxios, axios);
+};
